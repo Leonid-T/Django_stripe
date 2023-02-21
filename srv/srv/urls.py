97150ae03aefe6408c00aee0717d24bb3a412875
Leonid <titov281@yandex.ru>
2023-02-22 01:11:21 +0500

    Fix static

--- srv/srv/urls.py
+++ srv/srv/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('payment.urls')),
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
